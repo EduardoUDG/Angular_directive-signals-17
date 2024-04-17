3c4b3a310c44d3c9a00821f4cf136e57ba1836bb
eduardoudg <1510.lalo.51@gmail.com>
2024-04-17 17:22:54 -0600

    using signals

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
 import { UsersService } from '../../services/users.service';
 import { User } from '../../interfaces/users.interface';
 
@@ -15,6 +15,11 @@ export class UserInfoPageComponent implements OnInit {
   public currentUser  = signal<User|undefined>(undefined);
   public userWasFound = signal<boolean>(true);
 
+  public fullName     = computed<string>( () => {
+    if ( !this.currentUser() ) return 'Usuario no encontrado';
+    return `${this.currentUser()?.first_name} ${this.currentUser()?.last_name}`;
+  });
+
   ngOnInit(): void {
     this.loadUser( this.userId() );
   }
@@ -27,8 +32,15 @@ export class UserInfoPageComponent implements OnInit {
     this.userId.set(id);
 
     this._usersService.getById(id)
-      .subscribe( ({ data }) => {
-        this.currentUser.set( data );
+      .subscribe({
+        next: ({ data }) => {
+          this.currentUser.set( data );
+          this.userWasFound.set(true);
+        },
+        error: (error) => {
+          this.userWasFound.set(false);
+          this.currentUser.set(undefined);
+        }
       });
   }
 
