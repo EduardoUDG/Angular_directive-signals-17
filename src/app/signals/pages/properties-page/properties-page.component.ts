fd76b3067e36d13ee559b952e889d8f101a80a05
eduardoudg <1510.lalo.51@gmail.com>
2024-04-17 17:57:21 -0600

    properties

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from '../../interfaces/users.interface';
 
 @Component({
   selector: 'app-properties-page',
@@ -7,8 +8,41 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPageComponent {
 
-  onFieldUpdated( field:string, value:string ):void {
-    console.log({field, value});
+  public user = signal<User>({
+    id: 1,
+    email: "george.bluth@reqres.in",
+    first_name: "George",
+    last_name: "Bluth",
+    avatar: "https://reqres.in/img/faces/1-image.jpg"
+  });
+
+  onFieldUpdated( field:keyof User, value:string ):void {
+
+    this.user.update( current => {
+      switch( field ) {
+        case 'email':
+          current.email = value;
+          break;
+        case 'avatar':
+          current.avatar = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'id':
+          current.id = Number(value);
+          break;
+      }
+      return current;
+    });
+
+    // this.user.update( currentUser => ({
+    //   ...currentUser,
+    //   [field]: value
+    // }));
   }
 
 }
